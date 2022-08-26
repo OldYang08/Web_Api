64f66b48acffac7858a83be124b00d8c79126458
OldYang <homkins8@gmail.com>
2022-08-26 09:59:26 +0800

    add CustomerController

---
+++ Api_CRUD/Api_CRUD/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Api_CRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_CRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        private readonly CustomerDbContext dbContext;
+
+        public CustomerController(CustomerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(dbContext.Customers.ToList());
+        }
+    }
+}
