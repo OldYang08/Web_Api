6471f4e760c4d330f7127ead8442fd054c349a26
OldYang08 <107825148+OldYang08@users.noreply.github.com>
2022-08-26 09:45:08 +0800

    Add file
    Add file

---
+++ Api_CRUD/Api_CRUD/Data/CustomerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Api_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_CRUD.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
